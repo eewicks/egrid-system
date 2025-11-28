07b827894bc53782f2fb99e0f1b031a7cf8df16c
Erick <johnerickftambis@gmail.com>
2025-11-29 02:24:01 +0800

    Added Twilio SMS alert system

--- app/Http/Controllers/TwilioSMSController.php
+++ app/Http/Controllers/TwilioSMSController.php
@@ -3,18 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Twilio\Rest\Client;
 
 class TwilioSMSController extends Controller
 {
-     public function sendAlertSMS($deviceId, $messageText)
+  public static function sendAlertSMS($to, $messageText)
     {
-        $sid    = env('TWILIO_SID');
-        $token  = env('TWILIO_AUTH_TOKEN');
-        $from   = env('TWILIO_NUMBER');
-        $to     = env('ALERT_PHONE_NUMBER'); // Philippine number
-
         try {
+            $sid   = env('TWILIO_SID');
+            $token = env('TWILIO_TOKEN');
+            $from  = env('TWILIO_FROM');
+
             $client = new Client($sid, $token);
 
             $client->messages->create($to, [
@@ -22,17 +22,13 @@ class TwilioSMSController extends Controller
                 'body' => $messageText
             ]);
 
-            return response()->json(['success' => true, 'message' => 'SMS Sent']);
+            \Log::info("SMS sent to $to");
+
+            return true;
 
         } catch (\Exception $e) {
-            return response()->json(['error' => $e->getMessage()], 500);
+            \Log::error("TWILIO ERROR: ".$e->getMessage());
+            return false;
         }
     }
-
-    public function testSMS()
-    {
-        return $this->sendAlertSMS('TEST_DEVICE', 'Twilio Test SMS: Your system is working!');
-    }
-
-
 }
